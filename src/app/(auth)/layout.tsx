c084c0ebbecd501a86f8c2793cf379e8ae86c765
AreYouKhush <71877944+AreYouKhush@users.noreply.github.com>
2024-08-05 15:14:04 +0530

    create: login google auth

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,8 +1,34 @@
+"use client";
+
+import { logIn, logOut } from "@/redux/features/auth-slice";
+import { AppDispatch } from "@/redux/store";
+import { jwtDecode } from "jwt-decode";
+import { useCookies } from "next-client-cookies";
+import { redirect } from "next/navigation";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
 const LoginLayout = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
+  const dispatch = useDispatch<AppDispatch>();
+  const cookies = useCookies();
+  const token = cookies.get("token");
+
+  useEffect(() => {
+    if (token) {
+      const decoded = jwtDecode<any>(token);
+      if (Date.now() / 1000 > decoded.exp) {
+        dispatch(logOut());
+        console.log("Expired");
+      }
+      dispatch(logIn(decoded));
+      redirect("/dashboard");
+    }
+  }, []);
+
   return (
     <div>
       <main>{children}</main>
--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,10 +1,30 @@
 "use client";
 
+import { jwtDecode, JwtPayload } from "jwt-decode";
 import { Footer } from "./_components/Footer";
 import { LoginSignUp } from "./_components/LoginSignUp";
 import { Navbar } from "./_components/Navbar";
+import { redirect, useSearchParams } from "next/navigation";
+import { logIn, logOut } from "@/redux/features/auth-slice";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "@/redux/store";
+import { useCookies } from "next-client-cookies";
+import { useEffect } from "react";
 
 const LoginPage = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const cookies = useCookies();
+  const searchParams = useSearchParams();
+  const token = searchParams.get("token");
+
+  useEffect(() => {
+    if (token) {
+      const decoded = jwtDecode<any>(token);
+      dispatch(logIn(decoded));
+      cookies.set("token", token);
+      redirect("/dashboard");
+    }
+  }, []);
   return (
     <>
       <div className="bg-black flex flex-col justify-between items-center h-dvh">
